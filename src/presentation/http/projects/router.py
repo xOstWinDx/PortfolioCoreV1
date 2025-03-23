85f2e232eaa1769a7575ba5dd18635286772bde2
Alexey <Starobogatov.a@yandex.ru>
2025-03-23 11:14:01 +0300

    replace depends

--- src/presentation/http/projects/router.py
+++ src/presentation/http/projects/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 
 from src.application.usecases.add_project import AddNewProjectUseCase
 from src.infrastructure.schemas.project import ProjectResponse, ProjectSchema
-from src.presentation.http.dependencies.projects import get_create_project_use_case
+from src.presentation.http.projects.dependencies import get_create_project_use_case
 
 router = APIRouter(prefix="/projects", tags=["projects"])
 
