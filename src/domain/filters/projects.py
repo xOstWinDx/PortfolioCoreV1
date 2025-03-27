aad7435c6bf30c3190b3438163ec4eb0ab495f5a
Alexey <Starobogatov.a@yandex.ru>
2025-03-27 03:18:27 +0300

    rename

--- src/domain/filters/projects.py
+++ src/domain/filters/projects.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 from src.domain.filters.base import BaseFilter
 
 
 class ProjectFilter(BaseFilter):
     stack: list[str] | None = None
+    date_from: datetime | None = None
+    date_to: datetime | None = None
