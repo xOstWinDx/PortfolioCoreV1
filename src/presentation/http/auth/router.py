2addb5d60460a7ab80756714d57dadc459a25af8
Alexey <Starobogatov.a@yandex.ru>
2025-03-28 22:37:41 +0300

    change fastapi dep to dependency-injector dep

--- src/presentation/http/auth/router.py
+++ src/presentation/http/auth/router.py
@@ -1,29 +1,40 @@
 from typing import Annotated
 
+from dependency_injector.wiring import inject, Provide
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi import Request
-from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
 from starlette.responses import JSONResponse
 from user_agents import parse
 
 from src.application.usecases.login import LoginUseCase
 from src.application.usecases.update_token import UpdateTokenUseCase
+from src.container import container
 from src.domain.entities.tokens import RefreshToken
+from src.domain.exceptions.auth import TokenError
 from src.presentation.http.auth.dependencies import (
-    get_login_use_case,
     get_update_token_use_case,
-    validate_refresh_token,
 )
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
+container.wire(modules=[__name__])
+
+
+@router.post("/register")
+@inject
+async def register() -> (
+    None
+):  # TODO: сделать форму для регистрации с Email (наследник нижней)
+    pass
+
 
 @router.post("/login")
+@inject
 async def login(  # type: ignore
     request: Request,
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    use_case: Annotated[LoginUseCase, Depends(get_login_use_case)],
+    form_data: Annotated[..., Depends()],  # TODO: сделать схему для входа с Email
+    use_case: Annotated[LoginUseCase, Provide[container.login_use_case]],
 ):
     # Извлекаем User-Agent из заголовков
     user_agent_string = request.headers.get("User-Agent", "")
@@ -34,7 +45,7 @@ async def login(  # type: ignore
     browser = user_agent.browser.family  # Например, "Chrome", "Firefox", "Safari"
 
     tokens = await use_case(
-        username=form_data.username,
+        email=form_data.username,
         password=form_data.password,
         ip=request.client.host,
         platform=platform,
@@ -46,21 +57,22 @@ async def login(  # type: ignore
         content={"access_token": tokens[0], "refresh_token": tokens[1]}
     )
     response.set_cookie(
-        key="refresh_token", value=tokens[1], httponly=True, secure=True
-    )
-    response.set_cookie(
         key="access_token",
         value=tokens[0],
         httponly=True,
+    )
+    response.set_cookie(
+        key="refresh_token", value=tokens[1], httponly=True, secure=True
     )
     return response
 
 
 @router.post("/update_token")
+@inject
 async def update_token(  # type: ignore
     request: Request,
     use_case: Annotated[UpdateTokenUseCase, Depends(get_update_token_use_case)],
-    refresh_token: Annotated[RefreshToken, Depends(validate_refresh_token)],
+    refresh_token: Annotated[RefreshToken, Provide[container.update_token_use_case]],
 ):
     if refresh_token is None:
         raise HTTPException(status_code=401, detail="Refresh token not found")
@@ -72,20 +84,16 @@ async def update_token(  # type: ignore
     # Определяем платформу и браузер
     platform = user_agent.os.family  # Например, "Windows", "iOS", "Android"
     browser = user_agent.browser.family  # Например, "Chrome", "Firefox", "Safari"
-    token = await use_case(
-        ip=request.client.host,
-        platform=platform,
-        browser=browser,
-        refresh_token=refresh_token.token,
-    )
-    if token is None:
-        # Удаляем куку "token"
-        headers = {"Set-Cookie": "refresh_token=;"}
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect refresh token",
-            headers=headers,
+    try:
+        token = await use_case(
+            ip=request.client.host,
+            platform=platform,
+            browser=browser,
+            refresh_token=refresh_token.token,
         )
+    except TokenError as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=e.msg)
+
     response = JSONResponse(content={"access_token": token})
     response.set_cookie(
         key="access_token",
