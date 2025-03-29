6e87248d7275a9066b5801bec71834b7c17b2253
Alexey <Starobogatov.a@yandex.ru>
2025-03-29 22:53:38 +0300

    add new auth layer

--- src/presentation/http/projects/router.py
+++ src/presentation/http/projects/router.py
@@ -1,16 +1,37 @@
-from fastapi import APIRouter, Depends
+from typing import Annotated
+
+from dependency_injector.wiring import inject
+from fastapi import APIRouter, HTTPException, Depends
+from starlette import status
 
 from src.application.usecases.add_project import AddNewProjectUseCase
+from src.domain.exceptions.base import ConflictException
 from src.infrastructure.schemas.project import ProjectResponse, ProjectSchema
-from src.presentation.http.projects.dependencies import get_create_project_use_case
+from src.presentation.http.dependencies import access_token_schema
+from src.presentation.http.projects.dependencies import container
 
 router = APIRouter(prefix="/projects", tags=["projects"])
 
+container.wire(modules=[__name__])
 
-@router.post("/")
+
+@router.post("/", status_code=201)
+@inject
 async def create_project(
     project: ProjectSchema,
-    use_case: AddNewProjectUseCase = Depends(get_create_project_use_case),
+    use_case: Annotated[AddNewProjectUseCase, container.create_project_use_case],
+    access_token: Annotated[str, Depends(access_token_schema)],
 ) -> ProjectResponse:
-    res = await use_case(project.to_domain())
+    try:
+        res = await use_case(
+            project.to_domain(),
+            credentials=container.credentials(
+                access_token=access_token, refresh_token=""
+            ),
+        )
+    except ConflictException as e:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e))
+    except PermissionError as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+
     return ProjectResponse.model_validate(res, from_attributes=True)
